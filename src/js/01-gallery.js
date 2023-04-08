1ac4d3de3e85fe67188dfd551d20dc9ba7fbe5d3
YazikovXIII <vladzaraza@gmail.com>
2023-04-08 18:40:10 +0300

    123

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,8 +1,29 @@
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
-
-// Add imports above this line
 import { galleryItems } from './gallery-items';
-// Change code below this line
 
-console.log(galleryItems);
+
+const galleryBox = document.querySelector('.gallery');
+
+const elArr = galleryItems
+  .map(
+    item => `<li class="gallery__item">
+  <a class="gallery__link" href="${item.original}">
+    <img
+      class="gallery__image"
+      src="${item.preview}"
+      alt="${item.description}"
+    />
+  </a>
+</li>`
+  )
+  .join('');
+
+galleryBox.insertAdjacentHTML('beforeend', elArr);
+galleryBox.style.cssText = 'list-style: none;';
+
+const someGallery = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+  overlayOpacity: 1,
+});
