9a9645395d39c08ac40001a04c1f241a0c621bd5
YazikovXIII <vladzaraza@gmail.com>
2023-04-08 20:32:55 +0300

    123

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,17 @@
+import Player from '@vimeo/player';
 
+const player = new Player('vimeo-player');
+
+const playedTime = localStorage.getItem('videoplayer-current-time');
+if (playedTime) {
+  player.setCurrentTime(playedTime);
+}
+
+player.on('timeupdate', timeCheck);
+
+function timeCheck(data) {
+  localStorage.setItem(
+    'videoplayer-current-time',
+    JSON.stringify(data.seconds)
+  );
+}
